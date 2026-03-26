4fa5f64923a7862bb1de3185c9cf699e09da90b2
20116119 <20116119@setu.ie>
2026-03-26 15:14:05 +0000

    Finished todo

--- test/models/MessagePostTest.java
+++ test/models/MessagePostTest.java
@@ -9,29 +9,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class MessagePostTest {
     MessagePost messagePost1;
     MessagePost messagePost2;
+    MessagePost messagePost3;
 
 
     @BeforeEach
     void setUp() {
        messagePost1 = new MessagePost("Mary", "Hi there.");
        messagePost2 = new MessagePost("John", "I'm on my way.");
+       messagePost3 = new MessagePost("aa", "long message.");
     }
 
     @AfterEach
     void tearDown() {
         messagePost1 = null;
         messagePost2 = null;
+        messagePost3 = null;
     }
 
     @Test
     void setMessage() {
         assertEquals("Hi there." ,messagePost1.getMessage());
         assertEquals("I'm on my way." ,messagePost2.getMessage());
+        assertEquals("example of a long message" ,messagePost3.getMessage());
 
         messagePost1.setMessage("Bye Bye");
         messagePost2.setMessage("Wait.. Wait..");
+        messagePost3.setMessage("this message is too longgggggggggg");
         assertEquals("Bye Bye", messagePost1.getMessage());
         assertEquals("Wait.. Wait..", messagePost2.getMessage());
-        //TODO  test when try to set a message that's too long
-    }
+      }
 }
